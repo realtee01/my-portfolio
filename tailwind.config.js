c341fb2889b4885bbc537473d41e9f6b96e0ee04
realtee01 <tobilobaakala@gmail.com>
2026-04-01 13:39:27 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#0a0a0a',
+        'card-bg': '#121212',
+      }
+    },
+  },
+  plugins: [],
+}
