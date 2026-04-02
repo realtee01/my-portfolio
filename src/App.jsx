1b5332e681182f050a5e36e3e903989966251876
realtee01 <tobilobaakala@gmail.com>
2026-04-02 01:12:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function App() {
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {projects.map((project) => (
-              <Card key={project.id} {...project} />
+              <Card key={projects.id} {...projects} />
             ))}
           </div>
         </section> {/* End Projects */}
